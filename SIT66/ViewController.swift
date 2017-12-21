68ab29a301ddd30acc5ad86d0bdb25dcbb07ea76
Buzzeh <benitez@vt.edu>
2017-12-20 23:55:25 -0500

    Initial commit

--- SIT66/ViewController.swift
+++ SIT66/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //The webView loading all happens here
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let url:URL = URL(string: "https://shoulditakei66.today/")!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
